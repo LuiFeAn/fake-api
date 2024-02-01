3969703163b73b36e39d8ae531777478201c341f
luifean <luisfernandogvv@gmail.com>
2024-02-01 12:05:32 -0300

    fix: Ajustando testes referentes a criação de um produto

--- src/application/use_cases/product/test/create-product.spec.ts
+++ src/application/use_cases/product/test/create-product.spec.ts
@@ -1,12 +1,23 @@
+import { InMemoryProductsRepository } from "test/repositories/in-memory-products-repository";
 import inMemoryProductsRepository from "../../../../../test/factories/in-memory-product-repository";
 import { productFactory } from "../../../factories/product";
 
-
 describe("Testes de criação de produto", () => {
 
-    it("Deve criar um novo produto", () => {
+    let productInMemoryRepository: InMemoryProductsRepository;
 
-        const productInMemoryRepository = inMemoryProductsRepository();
+    beforeEach( () => {
+        productInMemoryRepository = inMemoryProductsRepository();
+
+    });
+
+    afterEach( () => {
+
+        productInMemoryRepository.products = [];
+
+    })
+
+    it("Deve criar um novo produto", async () => {
 
         const product = productFactory({
             title:"Novo produto",
@@ -14,34 +25,30 @@ describe("Testes de criação de produto", () => {
             value:200
         });
 
-        productInMemoryRepository.create(product)
+        await productInMemoryRepository.create(product)
 
-        expect(productInMemoryRepository.products.length).toBeGreaterThanOrEqual(1);
+        expect(productInMemoryRepository.products.length).toBe(1)
 
 
     });
 
     it("Não deve criar um produto caso o título não possua ao menos um caracter", () => {
 
-        const product = productFactory({
+        expect(() => productFactory({
             title:"",
             description:"Descrição Interessante",
             value:200
-        });
-
-        expect(product).toThrow()
+        })).toThrow()
 
     });
 
     it("Não deve criar um produto caso a descrição não possua ao menos um caracter", () => {
 
-        const product = productFactory({
+        expect(() => productFactory({
             title:"Título do Produto",
             description:"",
             value:200
-        });
-
-        expect(product).toThrow();
+        })).toThrow();
 
     });
 
