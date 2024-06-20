0cfcb4c82570cdfaae2c10972e6779f21bb6082e
luifean <luisfernandogvv@gmail.com>
2024-06-20 16:26:37 -0300

    feat: static module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,8 @@ import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from './infra/database/database.module';
 import { ProductsModule } from './infra/http/products/products.module';
 import { UseCasesProxyModule } from './infra/use_cases/proxy.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
   imports: [
@@ -10,6 +12,10 @@ import { UseCasesProxyModule } from './infra/use_cases/proxy.module';
       isGlobal: true,
     }),
     DatabaseModule,
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', '..', 'uploads', 'images', 'products'),
+      serveRoot: '/uploads/images/products',
+    }),
     UseCasesProxyModule.register(),
     ProductsModule,
   ],
